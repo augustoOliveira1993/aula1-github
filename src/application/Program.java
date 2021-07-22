8b849ec0b9a367d7c9a452976d1f917ae663f8d2
augusto <gutim160@gmail.com>
2021-07-22 18:28:22 -0300

    Adicionando coisas

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,17 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.print("Nome: ");
+		String nome = sc.nextLine();
+		
+		System.out.println("Seu nome é " + nome);
+		sc.close();
 
 	}
 
